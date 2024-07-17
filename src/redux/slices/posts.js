83605bfa59928fda556da1c93c944502ef994565
venjerin <vldvrb@bk.ru>
2024-07-17 20:42:14 +0300

    tags

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -6,6 +6,10 @@ export const fetchPosts = createAsyncThunk('/posts/fetchPosts', async() => {
     return data
 })
 
+export const fetchTags = createAsyncThunk('/posts/fetchTags', async() => {
+    const { data } = await axios.get('/tags') 
+    return data
+})
 
 const initialState = {
     posts: {
@@ -34,6 +38,18 @@ const postsSlice = createSlice({
         [fetchPosts.rejected]: (state, action) => {
             state.posts.items = []
             state.posts.status = 'error'
+        },
+        [fetchTags.pending]: (state, action) => {
+            state.tags.items = []
+            state.tags.status = 'loading'
+        },
+        [fetchTags.fulfilled]: (state, action) => {
+            state.tags.items = action.payload
+            state.tags.status = 'loaded'
+        },
+        [fetchTags.rejected]: (state, action) => {
+            state.tags.items = []
+            state.tags.status = 'error'
         }
     }
 })
