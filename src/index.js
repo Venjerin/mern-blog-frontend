c4f5430d5750439ecc2854df415d7b1af04f6d09
venjerin <vldvrb@bk.ru>
2024-07-17 19:23:53 +0300

    Redux toolkit + slices

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -15,7 +17,9 @@ root.render(
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-      <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
